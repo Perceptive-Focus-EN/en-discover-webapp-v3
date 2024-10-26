981e3b1dfb5306990331e3a4f012eee3b12f29f7
Perceptive Focus, LLC <info@perceptivefocusinc.com>
2024-10-26 16:32:01 -0700

    commented out access key institute

--- src/constants/Feed/connections.ts
+++ src/constants/Feed/connections.ts
@@ -1,10 +1,10 @@
-import { postReactionsApi } from '@/lib/api_s/reactions/postReactions';
-import { feedApi } from '../../lib/api_s/feed/index';
-import { axiosInstanc } from '../../lib/axiosSetup';
+// import { postReactionsApi } from '@/lib/api_s/reactions/postReactions';
+// import { feedApi } from '../../lib/api_s/feed/index';
+// import { axiosInstanc } from '../../lib/axiosSetup';
 
-export const FEED_ACTIONS = {
-  SEND_CONNECTION_REQUEST: feedApi.sendConnectionRequest,
-  ACCEPT_CONNECTION_REQUEST: feedApi.acceptConnectionRequest,
-  GET_CONNECTIONS: postReactionsApi.getConnections,
-  GET_CONNECTION_REQUESTS: connectionsApi.getConnectionRequests,
-};
+// export const FEED_ACTIONS = {
+//   SEND_CONNECTION_REQUEST: feedApi.sendConnectionRequest,
+//   ACCEPT_CONNECTION_REQUEST: feedApi.acceptConnectionRequest,
+//   GET_CONNECTIONS: postReactionsApi.getConnections,
+//   GET_CONNECTION_REQUESTS: connectionsApi.getConnectionRequests,
+// };
